d3eddb794c7c34bcca939bd86565604336ad8ca8
Fabian-MG <mirandafabian1510@gmail.com>
2020-09-16 01:00:22 -0500

    Adding interfaces, custom pipe and filter searchbar

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ɵConsole } from '@angular/core';
+import { IProduct } from './products';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   title: string = 'Product List';
   imageWidth: number = 50;
   imageHeigth: number = 2;
   showImage: Boolean = false;
-  listFilter: string = 'cart';
 
-  products: any[] = [
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.products;
+  }
+
+  filteredProducts: IProduct[];
+  products: IProduct[] = [
     {
       productId: 1,
       productName: 'Leaf Rake',
@@ -42,9 +56,45 @@ export class ProductListComponent {
       starRating: 4.8,
       imageUrl: 'assets/images/hammer.png',
     },
+    {
+      productId: 8,
+      productName: 'Saw',
+      productCode: 'TBX-0022',
+      releaseDate: 'May 15, 2019',
+      description: '15-inch steel blade hand saw',
+      price: 11.55,
+      starRating: 3.7,
+      imageUrl: 'assets/images/saw.png',
+    },
+    {
+      productId: 10,
+      productName: 'Video Game Controller',
+      productCode: 'GMG-0042',
+      releaseDate: 'October 15, 2018',
+      description: 'Standard two-button video game controller',
+      price: 35.95,
+      starRating: 4.6,
+      imageUrl: 'assets/images/xbox-controller.png',
+    },
   ];
+
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) => {
+      return product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1;
+    });
+  }
 
   toggleImage() {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit() {
+    console.log('in onit');
   }
 }
